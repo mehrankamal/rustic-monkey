195cf4d7712a069f7340cba2c9cc7a4a79ab9cd0
Mehran Kamal <29648490+mehrankamal@users.noreply.github.com>
2023-10-13 18:50:06 +0500

    Add welcome message in main repl.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::{env, io};
+use std::io::Write;
 
 mod token;
 mod lexer;
@@ -7,6 +8,11 @@ mod repl;
 fn main() {
     let stdin = io::stdin();
     let stdout = io::stdout();
+
+    let user = env::var("USER").unwrap();
+
+    write!(stdout.lock(), "Hello {}! This is the Monkey programming language!\n", user).unwrap();
+    write!(stdout.lock(), "Feel free to type in commands\n").unwrap();
 
     if let Err(e) = repl::start(stdin.lock(), stdout.lock()) {
         eprintln!("Error: {}", e);
